7fec682fa818b1d03eca6a50c4ed51c5b8a6c29f
Felix Gaertner <felix.gaertner@stud.htw-dresden.de>
2025-10-20 15:34:12 +0200

    Grundkarte implementiert und erste Struktur des Projektes erstellt

---
+++ WebGIS Test frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+
+import { onMounted } from 'vue'
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+onMounted(() => {
+  const map = L.map('map').setView([52.52, 13.405], 13) 
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; OpenStreetMap contributors'
+  }).addTo(map)
+})
+</script>
+
+<template>
+  <div id="map" class="w-screen h-screen mt-6"></div>
+</template>
+
+
